0b620a1d58db387b5a103676f24a0b86b10e8316
Egert Vreindlik <egert.vreindlik@tptlive.ee>
2022-11-08 14:10:53 +0200

    added questions

---
+++ question.js
@@ -0,0 +1,24 @@
+import inquirer from "inquirer";
+inquirer
+    .prompt([
+        {type:'input', name:'age', message: 'How old are you?'},
+        {type:'number', name:'eyes', message: 'How many eyes do you have?'},
+        {type:'confirm', name:'ask', message: 'Are you alive?'},
+        {type:'list', name:'gender', message: 'What gender are you?', choices:["male", "female", "NonBinary"]},
+        {type:'rawlist', name:'food', message: 'Which food would you choose to eat?', choices:["pizza", "burger", "salad", "noodles", "cheese", "meat"]},
+        {type:'expand', name:'feeling', message: 'Are you okay?',key:"y", choices:["yes", "no","maybe"]},
+        {type:'checkbox', name:'age', message: 'Do you like any of these?'},
+        {type:'password', name:'age', message: 'Tell me something important.'},
+        {type:'editor', name:'age', message: 'How old are you?'},
+    ])
+    .then((answer)=>{
+        console.log(`I am ${answer.age} years old`); 
+        console.log(`I have ${answer.eyes} eyes`);
+        console.log(`${answer.ask}, I have a phone`);
+        console.log(`I am ${answer.gender}`);
+        console.log(`I like ${answer.food}`);
+        console.log(`${answer.feeling}`);
+        console.log(`I have ${answer.eyes} eyes`);
+        console.log(`I have ${answer.eyes} eyes`);
+        console.log(`I have ${answer.eyes} eyes`);
+    })
