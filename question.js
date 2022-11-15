44229e17112d2ed025fdc1311c139a3d32dfb40f
Egert Vreindlik <egert.vreindlik@tptlive.ee>
2022-11-15 14:04:29 +0200

    didnt do much

--- question.js
+++ question.js
@@ -7,9 +7,9 @@ inquirer
         {type:'list', name:'gender', message: 'What gender are you?', choices:["male", "female", "NonBinary"]},
         {type:'rawlist', name:'food', message: 'Which food would you choose to eat?', choices:["pizza", "burger", "salad", "noodles", "cheese", "meat"]},
         {type:'expand', name:'feeling', message: 'Are you okay?',key:"y", choices:["yes", "no","maybe"]},
-        {type:'checkbox', name:'age', message: 'Do you like any of these?'},
-        {type:'password', name:'age', message: 'Tell me something important.'},
-        {type:'editor', name:'age', message: 'How old are you?'},
+        {type:'checkbox', name:'like', message: 'Do you like any of these?'},
+        {type:'password', name:'important', message: 'Tell me something important.'},
+        {type:'editor', name:'', message: 'How old are you?'},
     ])
     .then((answer)=>{
         console.log(`I am ${answer.age} years old`); 
@@ -18,7 +18,7 @@ inquirer
         console.log(`I am ${answer.gender}`);
         console.log(`I like ${answer.food}`);
         console.log(`${answer.feeling}`);
-        console.log(`I have ${answer.eyes} eyes`);
-        console.log(`I have ${answer.eyes} eyes`);
+        console.log(`Yes, I like ${answer.like}`);
+        console.log(`${answer.important}`);
         console.log(`I have ${answer.eyes} eyes`);
     })
